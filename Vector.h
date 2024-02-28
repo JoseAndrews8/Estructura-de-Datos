0f45c91785c874b456b2101353193c2c4c75749e
JoseAndrews8 <141264032+JoseAndrews8@users.noreply.github.com>
2024-02-28 13:52:03 -0400

    Create Vector.h

---
+++ Vector.h
@@ -0,0 +1,15 @@
+class Vector
+{
+private:
+	int vec[10], n;
+public:
+	Vector(void); //constructor
+	~Vector(void); // destructor
+
+	void set_n(int _n);
+	int get_n();
+
+	void cargarVector(int vec[], int n);
+	void mostrarVector(int vec[], int n);
+	void ordenarVector(int vec[], int n);
+};
